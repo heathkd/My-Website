70a4e51268184dc9711ec0a974134e4a15c31cfd
heathkd <heather.dsouza@hotmail.com>
2018-08-13 18:31:49 -0400

    fix nav-bar hyperlink locations

--- js/app.js
+++ js/app.js
@@ -1,2 +1,32 @@
 //custom javascript goes here
-$(document).foundation()	
+$(document).foundation();
+console.log("is this working?")
+
+function changeColour() {
+	var currentColour = document.getElementById("my-button").style.background;
+	var newColour;
+	console.log(currentColour);
+	if (currentColour === "red") {
+		newColour = "black";
+	}
+	else {
+		newColour = "red"
+	}
+	console.log(newColour);
+	document.getElementById("my-button").style.background = newColour;
+}
+
+function hide() {
+	document.getElementsByClassName("button")[0].style.display = "none";
+}
+
+function  alertYourUser(arg) {
+	var element = document.getElementById(arg);
+	alert(`Hey! You just clicked the ${arg} button!`)
+}
+
+// $(document).ready(function(){
+//     $('.button button').click(function() {	
+//     	$('.button button').hide();	
+// 	});	
+// });
