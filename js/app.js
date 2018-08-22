26ef1ca9c45caeb4d1c03b262b415ceeae861ff1
heathkd <heather.dsouza@hotmail.com>
2018-08-22 14:33:01 -0400

    rotate gear while scrolling

--- js/app.js
+++ js/app.js
@@ -1,29 +1,38 @@
 //custom javascript goes here
 $(document).foundation();
-console.log("is this working?")
 
-function changeColour() {
-	var currentColour = document.getElementById("my-button").style.background;
-	var newColour;
-	console.log(currentColour);
-	if (currentColour === "red") {
-		newColour = "black";
-	}
-	else {
-		newColour = "red"
-	}
-	console.log(newColour);
-	document.getElementById("my-button").style.background = newColour;
-}
+var gear_small_screen = document.getElementById("nav-gear-small-screen");
+var gear_large_screen = document.getElementById("nav-gear-large-screen");
+window.addEventListener("scroll", function() {
+	var rotation = window.pageYOffset/8;
+    gear_small_screen.style.transform = "rotate("+rotation+"deg)";
+    gear_large_screen.style.transform = "rotate("+rotation+"deg)";
+});
+// $(document).foundation();
+// console.log("is this working?")
 
-function hide() {
-	document.getElementsByClassName("button")[0].style.display = "none";
-}
+// function changeColour() {
+// 	var currentColour = document.getElementById("my-button").style.background;
+// 	var newColour;
+// 	console.log(currentColour);
+// 	if (currentColour === "red") {
+// 		newColour = "black";
+// 	}
+// 	else {
+// 		newColour = "red"
+// 	}
+// 	console.log(newColour);
+// 	document.getElementById("my-button").style.background = newColour;
+// }
 
-function  alertYourUser(arg) {
-	var element = document.getElementById(arg);
-	alert(`Hey! You just clicked the ${arg} button!`)
-}
+// function hide() {
+// 	document.getElementsByClassName("button")[0].style.display = "none";
+// }
+
+// function  alertYourUser(arg) {
+// 	var element = document.getElementById(arg);
+// 	alert(`Hey! You just clicked the ${arg} button!`)
+// }
 
 // $(document).ready(function(){
 //     $('.button button').click(function() {	
